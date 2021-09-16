1a6918742f5ae04aa3c113f64b7ecbad7ba9fb0b
mconour <michaelconour@gmail.com>
2021-09-15 22:10:34 -0700

    complete cubes solution

---
+++ 03_buggy_code_part_1/script.js
@@ -0,0 +1,7 @@
+const cubes = (a) => a ** 3
+
+console.log((cubes(2), 8));
+console.log((cubes(3), 27));
+console.log((cubes(4), 64));
+console.log((cubes(5), 125));
+console.log((cubes(10), 1000));
