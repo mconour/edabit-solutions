d71f7c0e69ed64f8c99abbcb6fe91be08fb6032d
mconour <michaelconour@gmail.com>
2021-09-15 22:03:22 -0700

    complete triArea solution

--- 02_area_of_a_triangle/script.js
+++ 02_area_of_a_triangle/script.js
@@ -1,9 +1,7 @@
-const triArea = (base, height) => {
-    
-}
+const triArea = (base, height) => base * height / 2;
 
-console.log((triArea(3, 2), 3));
-console.log((triArea(5, 4), 10));
-console.log((triArea(10, 10), 50));
-console.log((triArea(0, 60), 0));
-console.log((triArea(12, 11), 66));
+console.log((triArea(3, 2)));
+console.log((triArea(5, 4)));
+console.log((triArea(10, 10)));
+console.log((triArea(0, 60)));
+console.log((triArea(12, 11)));
