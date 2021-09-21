edf60a8c639748e11d1c1e845ad13a42bfdd5532
mconour <michaelconour@gmail.com>
2021-09-21 08:52:14 -0700

    commit latest updates for project 4

---
+++ 04_return_the_first_element_in_an_array/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+console.log(getFirstValue([1, 2, 3]));
+console.log(getFirstValue([80, 5, 100]));
+console.log(getFirstValue([-500, 0, 50]));
+console.log(getFirstValue([5, 2, 3]));
+console.log(getFirstValue([75675, 5, 100]));
+console.log(getFirstValue([-52320, 0, 50]));
+
